abc89815f79e148a77bb31d355b8499eca71e2b9
Florent Flament <contact@florentflament.com>
2018-03-19 21:06:59 +0100

    Remove unused commented code

--- src/fx_turn.asm
+++ src/fx_turn.asm
@@ -171,11 +171,3 @@ fx_turn_shapes_h:
 ; Data
 	INCLUDE "fx_turn_data.asm"
 	INCLUDE "fx_turn_tables.asm"
-
-;fx_turn_pf:
-;	dc.b $00, $aa, $aa, $aa, $00, $54, $54, $54
-;	dc.b $00, $aa, $aa, $aa, $00, $54, $54, $54
-;	dc.b $00, $aa, $aa, $aa, $00, $54, $54, $54
-;	dc.b $00, $aa, $aa, $aa, $00, $54, $54, $54
-;	dc.b $00, $aa, $aa, $aa, $00, $54, $54, $54
-;	dc.b $00, $aa, $aa, $aa, $00, $00, $00, $00
